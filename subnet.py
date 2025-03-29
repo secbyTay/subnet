95db63a327409a5fd2eb0d8906773fe8cff9008e
Taylor M Smith <ts5570@nyu.edu>
2025-03-29 10:45:35 -0500

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,82 @@
+subnet_answers = {
+    "Problem 1": {
+        "IP": "207.65.177.61/29",
+        "Network Address": "207.65.177.56",
+        "First Usable": "207.65.177.57",
+        "Last Usable": "207.65.177.62",
+        "Broadcast Address": "207.65.177.63",
+        "Usable Hosts": 6
+    },
+    "Problem 2": {
+        "IP": "118.50.139.104/26",
+        "Network Address": "118.50.139.64",
+        "First Usable": "118.50.139.65",
+        "Last Usable": "118.50.139.126",
+        "Broadcast Address": "118.50.139.127",
+        "Usable Hosts": 62
+    },
+    "Problem 3": {
+        "IP": "29.35.101.146/24",
+        "Network Address": "29.35.101.0",
+        "First Usable": "29.35.101.1",
+        "Last Usable": "29.35.101.254",
+        "Broadcast Address": "29.35.101.255",
+        "Usable Hosts": 254
+    },
+    "Problem 4": {
+        "IP": "196.21.63.189/27",
+        "Network Address": "196.21.63.160",
+        "First Usable": "196.21.63.161",
+        "Last Usable": "196.21.63.190",
+        "Broadcast Address": "196.21.63.191",
+        "Usable Hosts": 30
+    },
+    "Problem 5": {
+        "IP": "107.6.24.231/25",
+        "Network Address": "107.6.24.128",
+        "First Usable": "107.6.24.129",
+        "Last Usable": "107.6.24.254",
+        "Broadcast Address": "107.6.24.255",
+        "Usable Hosts": 126
+    },
+    "Problem 6": {
+        "IP": "18.247.242.18/28",
+        "Network Address": "18.247.242.16",
+        "First Usable": "18.247.242.17",
+        "Last Usable": "18.247.242.30",
+        "Broadcast Address": "18.247.242.31",
+        "Usable Hosts": 14
+    },
+    "Problem 7": {
+        "IP": "185.232.204.60/26",
+        "Network Address": "185.232.204.0",
+        "First Usable": "185.232.204.1",
+        "Last Usable": "185.232.204.62",
+        "Broadcast Address": "185.232.204.63",
+        "Usable Hosts": 62
+    },
+    "Problem 8": {
+        "IP": "96.217.166.103/29",
+        "Network Address": "96.217.166.96",
+        "First Usable": "96.217.166.97",
+        "Last Usable": "96.217.166.102",
+        "Broadcast Address": "96.217.166.103",
+        "Usable Hosts": 6
+    },
+    "Problem 9": {
+        "IP": "7.202.127.145/27",
+        "Network Address": "7.202.127.128",
+        "First Usable": "7.202.127.129",
+        "Last Usable": "7.202.127.158",
+        "Broadcast Address": "7.202.127.159",
+        "Usable Hosts": 30
+    },
+    "Problem 10": {
+        "IP": "174.187.89.187/24",
+        "Network Address": "174.187.89.0",
+        "First Usable": "174.187.89.1",
+        "Last Usable": "174.187.89.254",
+        "Broadcast Address": "174.187.89.255",
+        "Usable Hosts": 254
+    }
+}
